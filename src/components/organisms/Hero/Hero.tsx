f30a14e8a5c3f85238151eb72dfd12b91363a91f
Mateusz Szweda <mateuszsz@huncwot.com>
2025-05-26 15:57:23 +0200

    handle webp responsive image

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,12 +1,20 @@
+import { ResponsiveImage } from '@/components/molecules/Image/ResponsiveImage';
 import { HeroBlock } from '@/services/api/api.types';
 
 export const Hero = ({ block }: { block: HeroBlock }) => {
+    console.log(block);
     return (
         <div className="relative bg-gray-900 text-white py-20">
-            {block.image && (
-                <div
-                    className="absolute inset-0 bg-cover bg-center"
-                    style={{ backgroundImage: `url(${block.image.url})` }}
+            {block.imageDesktop && (
+                <ResponsiveImage
+                    srcDesktop={block.imageDesktop?.webpUrl || block.imageDesktop?.url || ''}
+                    srcMobile={
+                        block.imageMobile?.webpUrl ||
+                        block.imageMobile?.url ||
+                        block.imageDesktop?.url ||
+                        ''
+                    }
+                    alt={block.imageDesktop?.alt || 'Image'}
                 />
             )}
             <div className="relative container mx-auto px-4">

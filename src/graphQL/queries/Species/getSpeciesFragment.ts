58f217b55142ff56e193ebe4f3a251801854830d
Mateusz Szweda <mateuszsz@huncwot.com>
2025-06-02 23:32:30 +0200

    species fix

--- src/graphQL/queries/Species/getSpeciesFragment.ts
+++ src/graphQL/queries/Species/getSpeciesFragment.ts
@@ -5,7 +5,7 @@ export const GET_SPECIES_FRAGMENT = gql`
     ${SPECIES_FRAGMENT}
 
     query GetSpecies($limit: Int, $page: Int) {
-        SpeciesFragment(limit: $limit, page: $page) {
+        allSpecies(limit: $limit, page: $page) {
             docs {
                 ...SpeciesFragment
             }

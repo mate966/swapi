510d604f8f3348795d35c2d0a9a879e92c789b7f
Mateusz Szweda <mateuszsz@huncwot.com>
2025-06-04 21:55:45 +0200

    init smootscroller

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@/components/scaffold/Header/Header';
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
 import { Footer } from './components/scaffold/Footer/Footer';
+import SmoothScroll from './utils/SmoothScroller/SmoothScroller';
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App: React.FC = () => {
     return (
         <>
-            <Header />
-            <main className="pt-16">
-                <Outlet />
-            </main>
-            <Footer />
+            <SmoothScroll>
+                <Header />
+                <main className="pt-16">
+                    <Outlet />
+                </main>
+                <Footer />
+            </SmoothScroll>
         </>
     );
 };

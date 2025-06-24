9bdf47119256e3bc124636667e283440291c4a68
Mateusz Szweda <mateuszsz@huncwot.com>
2025-06-24 23:22:02 +0200

    fix curtain transition

--- src/components/scaffold/PageTransition/index.tsx
+++ src/components/scaffold/PageTransition/index.tsx
@@ -39,7 +39,7 @@ export const PageTransition = () => {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: -20 }}
-                transition={{ duration: 0.3, delay: isCurtainVisible ? 0.6 : 0 }}
+                transition={{ duration: 0.6, delay: isCurtainVisible ? 0.6 : 0 }}
             >
                 {outlet}
             </motion.div>
